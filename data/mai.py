f99ab177abeb1eb1e996ac849793858cdf447860
Ilya Zawyalow <ilyazawyalow@gmail.com>
2023-10-03 16:39:56 +0300

    Без time.sleep() и count

--- data/mai.py
+++ data/mai.py
@@ -4,7 +4,8 @@ import os
 import requests
 import time
 from datetime import datetime, timedelta
+
 date_to = datetime.now()
 date_last = date_to - timedelta(days=30)
-pr = worker.Warker(date_last, date_to, 0, None)
+pr = worker.Warker(date_last, date_to)
 pr.run()
--- data/worker.py
+++ data/worker.py
@@ -10,13 +10,12 @@ ID_ROLES_LIST = ["156", "160", "10", "12", "150", "25", "165", "34", "36", "73",
 DEFAULT_MAX_REC_RETURNED = 2000
 URL = 'https://api.hh.ru/vacancies'
 DEFAULT_MAX_STEP_SIZE = 60 * 60
-DEFAULT_TIME_LIMIT = timedelta(seconds=1)/100
+
 
 class Warker:
-    def __init__(self, date_last, date_to, count, last_req_time):
+    def __init__(self, date_last, date_to):
         self.date_last = date_last
         self.date_to = date_to
-        self.count = count
 
     def api_req(self, page, date_from, date_to, retry=10):
         params = {
@@ -30,22 +29,15 @@ class Warker:
         except Exception as err:
             if retry:
                 time.sleep(3)
-                print('ошибкааааааааааааааааааааааааааааааааааа')
-                return self.api_req(page, date_from, date_to, retry = (retry - 1))
+                return self.api_req(page, date_from, date_to, retry=(retry - 1))
             else:
                 raise
         else:
             data = req.content.decode()
             data = json.loads(data)
-            self.count +=1
-            if self.count == 10:
-                time.sleep(0.6)
-                self.count = 0
             return data
         finally:
             req.close()
-
-
 
     def get_time_step(self, date_left, date_right):
         if date_left < 0:
@@ -64,12 +56,12 @@ class Warker:
         print('Найдено----->>', data['found'])
         return mid, data['pages']
 
-
     def comvert_time_in_seconds(self, data):
         return (data - self.date_last).days * 24 * 60 * 60
 
     def comvert_seconds_in_time(self, sec):
-        return self.date_last + timedelta(days=sec/(24 * 60 * 60))
+        return self.date_last + timedelta(days=sec / (24 * 60 * 60))
+
     def run(self):
         logger.info(f'Запуск парсера. Временной интервал: От {self.date_to} --> до --> {self.date_last}')
         self.date_to = self.comvert_time_in_seconds(self.date_to)
